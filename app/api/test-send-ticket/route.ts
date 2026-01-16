a9c8e4272cfc01dd9c1f5d346f30c2aa8e0ab991
iamsanka <iamsankadesilva@gmail.com>
2026-01-16 10:36:24 +0200

    font change

--- app/api/test-send-ticket/route.ts
+++ app/api/test-send-ticket/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  const testOrderId = "cmkgl90jo0001jr04aehus0mx";
+  const testOrderId = "cmkgm4ife0006b28w0936p5bd";
 
   const res = await fetch(
     `${process.env.NEXT_PUBLIC_URL}/api/send-ticket?orderId=${testOrderId}`
