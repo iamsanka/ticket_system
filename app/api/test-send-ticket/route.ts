7a12668740af34e504a40489d596b703f2c2f57b
iamsanka <iamsankadesilva@gmail.com>
2026-01-16 10:01:18 +0200

    testing email 1

--- app/api/test-send-ticket/route.ts
+++ app/api/test-send-ticket/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  const testOrderId = "cmkg0gowo0001b2o4dkh1m7pj";
+  const testOrderId = "cmkgl90jo0001jr04aehus0mx";
 
   const res = await fetch(
     `${process.env.NEXT_PUBLIC_URL}/api/send-ticket?orderId=${testOrderId}`
