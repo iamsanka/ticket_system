edcd3b7db76aaac7a2352377e116d358f1fb252b
iamsanka <iamsankadesilva@gmail.com>
2026-01-17 12:36:07 +0200

    add edenred

--- app/api/test-send-ticket/route.ts
+++ app/api/test-send-ticket/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  const testOrderId = "cmki5g7mn0001b2zgu0i0joca";
+  const testOrderId = "cmki68o8f0007l504a42sa48d";
 
   const res = await fetch(
     `${process.env.NEXT_PUBLIC_URL}/api/send-ticket?orderId=${testOrderId}`
