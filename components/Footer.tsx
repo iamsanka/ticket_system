a4aed203895fe2d99e1d635e25bd7dc7c79bd7f9
iamsanka <iamsankadesilva@gmail.com>
2026-01-21 22:50:31 +0200

    footer added 2

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,9 +4,10 @@ export default function Footer() {
       <div className="max-w-screen-xl mx-auto text-left space-y-1">
         <div>Taprobane Entertainment Oy</div>
         <div>Business ID: 3581857-4</div>
-        <div>Helsinki, Finland</div>
+        <div>Katontekijänkuja 1, 00700 Helsinki, Finland</div>
         <div>Email: info@taprobane.fi</div>
         <div>Website: www.taprobane.fi</div>
+        <div>Conatct No: +358 442363616 / +358 442363618</div>
         <div>© {new Date().getFullYear()} SanD. All Rights Reserved.</div>
       </div>
     </footer>
